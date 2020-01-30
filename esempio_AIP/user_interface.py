14589a0f4438dc85e1e80d795677e9bed1240ec1
Gioacchino Sterlicchio <gioacchinosterlicchio@gmail.com>
2020-01-30 20:56:33 +0100

    Aggiunti comandi per gestire la selezione dei sintomi

--- esempio_AIP/user_interface.py
+++ esempio_AIP/user_interface.py
@@ -2,39 +2,52 @@
 import tkinter as tk
 import ontology_manager as om
 
-def menuSynptoms(frame,symptoms):
-    mb =  tk.Menubutton (frame, text="Secelct symptoms", font=16, relief=tk.RIDGE, pady=10, padx=10, activebackground="light grey")
-    mb.pack(padx=10, pady=10)
-    mb.menu =  tk.Menu (mb)
-    mb["menu"] =  mb.menu
-    #file_symptoms = open("D:\\utente\\Documents\\IConProgetto\\esempio_AIP\\menu_sintomi.txt")
-    for i in symptoms:
-        mb.menu.add_checkbutton(label = i)
-    
-    return mb
+def selectSymptom():
+    stringS = varCheck.get()
+    if stringS != "null":
+        consoleArea.insert(tk.INSERT, stringS+"-")
 
+def send_symptom():
+    input = consoleArea.get("1.0", "end-2c")#legge da linea uno, carattere zero, -2c cancella 2 caratteri(- e newline)
+    input = input.split("-")
+    print(input)
+    #da qui aggiungere le funzioni per l'elaborazione della malattia
 
-def user_window(symptoms):
-    window = tk.Tk()
-    window.geometry("400x400")
-    window.resizable(0,0)
-    window.title("Human disease diagnosis")
+idoid = om.owl.get_ontology("D:\\utente\\Documents\\IConProgetto\\esempio_AIP\\inferred_doid")
+idoid.load()
+map_disease_symptom = om.build_map(idoid) #dizionario malattie,sintomi
+symptoms = om.list_symptoms(map_disease_symptom)
+_KB = om.create_KB(map_disease_symptom) #KB con clausole malattia(tetsa) e sintomi(corpo)
 
-    frame = tk.Frame(window)
-    frame.pack()
+window = tk.Tk()
+window.geometry("400x400")
+window.resizable(0,0)
+window.title("Human disease diagnosis")
 
-    mb = menuSynptoms(frame,symptoms)
-    buttomGo = tk.Button(frame, text = "Submit", font=14, activebackground="light grey", relief=tk.RIDGE)
-    buttomGo.pack(padx=10, pady=10)
-    
-    return window
+frame = tk.Frame(window)
+frame.pack()
 
+mb =  tk.Menubutton (frame, text="Select symptoms", font=12, relief=tk.RIDGE, pady=10, padx=10, activebackground="light grey")
+mb.pack(padx=10, pady=10)
+mb.menu =  tk.Menu (mb)
+mb["menu"] =  mb.menu
+varCheck = tk.StringVar()
+for i in symptoms:
+    mb.menu.add_checkbutton(label=i, onvalue=i, offvalue = "null", command=selectSymptom, variable=varCheck)
 
+buttomSubmit = tk.Button(frame, text = "Submit", font=12, activebackground="light grey", relief=tk.RIDGE, command=send_symptom)
+buttomSubmit.pack(padx=10, pady=10)
+
+labelConsole = tk.Label(frame, text="Console area:")
+labelConsole.pack(anchor=tk.W)
+consoleArea = tk.Text(frame, wrap=tk.WORD)
+consoleArea.pack()
+
+window.mainloop()
+
+"""
 if __name__ == "__main__":
-    idoid = om.owl.get_ontology("D:\\utente\\Documents\\IConProgetto\\esempio_AIP\\inferred_doid")
-    idoid.load()
-    map_disease_symptom = om.build_map(idoid) #dizionario malattie,sintomi
-    symptoms = om.list_symptoms(map_disease_symptom)
-    _KB = om.create_KB(map_disease_symptom) #KB con clausole malattia(tetsa) e sintomi(corpo)
+    
     window = user_window(symptoms)
     window.mainloop()
+"""
